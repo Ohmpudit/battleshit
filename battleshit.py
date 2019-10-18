a137d8e8c968cf5064253749594a566226c4aa8b
Ohmpudit <45118753+Ohmpudit@users.noreply.github.com>
2019-10-18 09:18:11 +0700

    Create battleshit.py

---
+++ battleshit.py
@@ -0,0 +1,18 @@
+import pygame
+from enum import Enum
+
+Class Direction(Enum):
+
+Class Ship:
+
+
+Class Board:
+
+#user input
+Class PlayerBoard(Board):
+
+#handle input &output
+Class Display:
+
+#control the game
+Class Game:
